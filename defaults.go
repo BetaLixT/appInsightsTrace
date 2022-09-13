888ca532c41b63aef46a7d11889702ab15b57454
Karim <karim.wael@gmail.com>
2022-09-13 11:47:26 +0400

    made default extractor extract trex.TxModel info

--- defaults.go
+++ defaults.go
@@ -1,13 +1,22 @@
 package appinsightstrace
 
-import "context"
+import (
+	"context"
+
+	"github.com/soreing/trex"
+)
 
 type DefaultTraceExtractor struct {
-  
 }
 
+const TRACE_INFO_KEY = "tinfo"
+
 func (*DefaultTraceExtractor) ExtractTraceInfo(
-		_ context.Context,
-	) (ver, tid, pid, rid, flg string) {
-	  return "", "", "", "", ""
+	ctx context.Context,
+) (ver, tid, pid, rid, flg string) {
+	if tinfo, ok := ctx.Value(TRACE_INFO_KEY).(trex.TxModel); !ok {
+		return "", "", "", "", ""
+	} else {
+		return tinfo.Ver, tinfo.Tid, tinfo.Pid, tinfo.Rid, tinfo.Flg
 	}
+}
